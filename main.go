b8c07b1f0a43eda801f5e3f2716f8780181eb9bc
priyantha gunasekara <priyantha.getc@gmail.com>
2022-11-24 21:13:59 +0530

    add a test api to go service

--- main.go
+++ main.go
@@ -7,9 +7,14 @@ import (
 
 func main() {
 	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/test/", TestAPI)
 	http.ListenAndServe(":8085", nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
+
+func TestAPI(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is a test API, response from , %s", r.URL.Path[1:])
 }
